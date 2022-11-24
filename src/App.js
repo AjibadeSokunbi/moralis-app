c75a2d61600766a95d9d93badcc4dcd59ea1d26e
Ajibade <ajibadesokunbi98@gmail.com>
2022-11-24 14:22:23 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+  const [address, setAddress] = useState("");
+  const [chain, setChain] = useState("0x1");
+  const [txs, setTxs] = useState(null);
+
+  async function fetchTxs() {
+    let res = await axios.get(`http://localhost:3000/txs`, {
+      params: { address: address, chain: chain },
+    });
+
+    console.log(res);
+
+    setTxs(res.data.result);
+  }
+
+  function addressChange(e) {
+    setAddress(e.target.value);
+    setTxs(null);
+  }
+
+  function chainChange(e) {
+    setChain(e.target.value);
+    setTxs(null);
+  }
+  const maxValue = (e) => {
+       (e.value / 1E18).toFixed(4)
+  }
+  console.log(maxValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div style={{ fontSize: "23px", fontWeight: "700" }}>
+          Get Wallet Native Transactions
+        </div>
+        <button className="bu" onClick={fetchTxs}>
+          Get Txs
+        </button>
+        <div className="inputs">
+          <div style={{ display: "flex" }}>
+            <div style={{ width: "80px" }}>Wallet:</div>
+
+            {/****  WALLET INPUT ****/}
+            <input
+              className="input"
+              value={address}
+              onChange={(e) => addressChange(e)}
+            ></input>
+          </div>
+          <div style={{ display: "flex" }}>
+            <div style={{ width: "80px" }}>Chain:</div>
+
+            {/**** CHAIN SELECTION ****/}
+            <select className="input" onChange={(e) => chainChange(e)}>
+              <option value="0x1">Ethereum</option>
+              <option value="0x13881">Mumbai</option>
+            </select>
+          </div>
+        </div>
+
+        {/**** Results ****/}
+
+        {txs && (
+          <div className="results">
+            {txs.result?.map((e, i) => {
+              return (
+              <a href={`https://mumbai.polygonscan.com/tx/${e.hash}`} key={i} className="tx" target="_blank" rel="noreferrer">
+                [{e.block_timestamp.slice(0,10)}]
+                {e.from_address.toLowerCase() === address.toLowerCase() ?
+                <span> Send {(e.value / 1E18).toFixed(4)} ETH {e.value.from_address }</span>  :
+                <span> Receive {(e.value / 1E18).toFixed(4)} ETH</span>
+              }
+              </a>
+              )
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
